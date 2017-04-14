117f358e260e365f4c0d93de5ad305c2abeb3b92
sourabh kulkarni <skulkar6@stevens.edu>
2017-04-14 18:13:27 -0400

    changes

--- app/src/main/java/com/example/sourabhkulkarni/prebootest/Login.java
+++ app/src/main/java/com/example/sourabhkulkarni/prebootest/Login.java
@@ -1,15 +1,18 @@
 package com.example.sourabhkulkarni.prebootest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     Button bLogin;
     EditText etPassword, etUsername;
+    TextView RegisterLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,21 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etUsername=(EditText)findViewById(R.id.etusername);
         etPassword=(EditText)findViewById(R.id.etpassword1);
         bLogin=(Button)findViewById(R.id.login);
+        RegisterLink=(TextView)findViewById(R.id.tvRegLink);
         bLogin.setOnClickListener(this);
+        RegisterLink.setOnClickListener(this);
     }
+
 
 
     public void onClick(View v){
         switch (v.getId()){
             case R.id.login:
+
+
+
+                break;
+            case R.id.tvRegLink:
 
 
 
--- app/src/main/java/com/example/sourabhkulkarni/prebootest/MainActivity.java
+++ app/src/main/java/com/example/sourabhkulkarni/prebootest/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.sourabhkulkarni.prebootest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button bLogout;
+    EditText etName, etage, etUsername;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etName = (EditText) findViewById(R.id.etname);
+        etage = (EditText) findViewById(R.id.etage);
+        etUsername = (EditText) findViewById(R.id.etusername1);
+
+        bLogout = (Button) findViewById(R.id.logout);
+        bLogout.setOnClickListener(this);
     }
-}
+
+        public void onClick(View v){
+        switch (v.getId()){
+            case R.id.logout:
+                startActivity(new Intent(this, Login.class));
+
+
+                break;
+
+
+
+        }
+    }
+    }
+
